56df2844bb59fa6bed07bf090783809ddf5eeaa6
hristo2612 <hristoapps@gmail.com>
2021-11-27 16:56:42 +0200

    Features Section

--- src/app/components/main/components/features-section/features-section.component.ts
+++ src/app/components/main/components/features-section/features-section.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesSectionComponent implements OnInit {
 
+  features: any[] = [
+    {
+      title: 'Explore the World',
+      description: 'Do you like to travel? Explore the world and find the best places to visit.',
+      iconClass: 'icon-basic-world'
+    },
+    {
+      title: 'Meet Nature',
+      description: 'Meet nature and share your experience with other people.',
+      iconClass: 'icon-basic-compass'
+    },
+    {
+      title: 'Find Your Way',
+      description: 'Find your way around the world with our map.',
+      iconClass: 'icon-basic-map'
+    },
+    {
+      title: 'Live A Healthier Life',
+      description: 'Live a healthier life with our tips and advice.',
+      iconClass: 'icon-basic-heart'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
