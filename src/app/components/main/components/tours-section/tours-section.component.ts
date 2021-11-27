f6666dc86430b7467d807206cd34aba45259a705
hristo2612 <hristoapps@gmail.com>
2021-11-27 18:54:24 +0200

    Tour Cards + Button

--- src/app/components/main/components/tours-section/tours-section.component.ts
+++ src/app/components/main/components/tours-section/tours-section.component.ts
@@ -8,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class ToursSectionComponent implements OnInit {
 
   tours: any[] = [
-    {},
-    {},
-    {}
+    {
+      title: 'The Sea Explorer',
+      price: 299,
+      list: [
+        '3 day tour',
+        'Up to 30 people',
+        '2 tour guides',
+        'Sleep in cozy hotels',
+        'Difficulty: Easy'
+      ]
+    },
+    {
+      title: 'The Forest Hiker',
+      price: 499,
+      list: [
+        '7 day tour',
+        'Up to 40 people',
+        '6 tour guides',
+        'Sleep in provided tents',
+        'Difficulty: Medium'
+      ]
+    },
+    {
+      title: 'The Snow Adventurer',
+      price: 999,
+      list: [
+        '5 day tour',
+        'Up to 15 people',
+        '3 tour guides',
+        'Sleep in provided tents',
+        'Difficulty: Hard'
+      ]
+    }
   ]
 
   constructor() { }
